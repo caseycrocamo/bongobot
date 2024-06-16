167bb3255bd4f31fa960a38a129260343067aa69
Casey Crocamo <casey.crocamo@gmail.com>
2024-06-16 01:24:22 -0700

    add getMaxLevelCraftingDisciplines

--- gw2/characters.js
+++ gw2/characters.js
@@ -6,8 +6,8 @@ async function getCraftingLevels(){
         console.log('getting characters');
         const characters = await getCharacterList();
         if(characters){
-            for(const character of characters){
-                console.log('getting crafting disciplines for character', character);
+            const promises = characters.map(async character => {
+                console.log('getting crafting professions for', character);
                 const professions = await getCraftingProfessions(encodeURI(character));
                 professions.crafting.forEach(profession => {
                     const name = profession.discipline;
@@ -20,15 +20,22 @@ async function getCraftingLevels(){
                         craftingLevels[name] = profession.rating;
                     }
                 });
-            }
+            });
+            await Promise.all(promises);
         }
         return craftingLevels;
     } catch (err){
         console.error(err);
     }
 }
-export async function getWeaponsmithLevel(){
+export async function getMaxLevelCraftingDisciplines(){
+    let disciplines = [];
     const craftingLevels = await getCraftingLevels();
-    console.log(craftingLevels);
+    Object.keys(craftingLevels).forEach((discipline) => {
+        if(craftingLevels[discipline] == 500){
+            disciplines.push(discipline);
+        }
+      });
+    return disciplines;
 }
-getWeaponsmithLevel();
+getMaxLevelCraftingDisciplines();
